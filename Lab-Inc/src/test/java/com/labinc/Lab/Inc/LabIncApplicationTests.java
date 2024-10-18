2c9801efd1fd2d96d823ca024247cbb1fcff137b
GabrielFilomeno <biel_floripa@hotmail.com>
2024-10-17 21:28:49 -0300

    criaçao-projeto

---
+++ Lab-Inc/src/test/java/com/labinc/Lab/Inc/LabIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labinc.Lab.Inc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
