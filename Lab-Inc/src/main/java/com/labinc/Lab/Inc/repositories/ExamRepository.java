c146530caf227472241691c1439624acefd5e5ba
Randy Gomes <raandy.gc@gmail.com>
2024-10-24 18:38:28 -0300

    feat/exam: cria repository
    - Cria o ExamRepository para operações de banco de dados.

---
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/repositories/ExamRepository.java
@@ -0,0 +1,8 @@
+package com.labinc.Lab.Inc.repositories;
+
+import com.labinc.Lab.Inc.entities.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+}
