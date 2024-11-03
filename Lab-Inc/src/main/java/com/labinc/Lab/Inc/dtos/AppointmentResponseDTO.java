cd6cd1bf4c576f660b9c972cb7235c12bfd33cc4
Randy Gomes <raandy.gc@gmail.com>
2024-11-03 01:54:43 -0300

    docs(appointment): adicionar documentação Swagger para Appointment
    - Adicionar anotações @Operation e @ApiResponses para documentar endpoints da API.
    
    - Adicionar anotações @Schema na Entity e nos Dtos referentes à Appointment.

--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/AppointmentResponseDTO.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/AppointmentResponseDTO.java
@@ -1,5 +1,7 @@
 package com.labinc.Lab.Inc.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +12,27 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Dados de resposta para uma consulta")
 public class AppointmentResponseDTO {
+
+        @Schema(description = "ID da consulta", example = "1")
         private Long appointmentId;
+
+        @Schema(description = "Razão da consulta", example = "Rotina")
         private String reason;
+
+        @Schema(description = "Data da consulta", example = "15/10/2024")
         private LocalDate consultDate;
+
+        @Schema(description = "Hora da consulta", example = "14:30")
         private LocalTime consultTime;
+
+        @Schema(description = "Descrição do problema", example = "Dor de cabeça persistente")
         private String problemDescrip;
+
+        @Schema(description = "Medicamentos prescritos", example = "Paracetamol")
         private String prescMed;
+
+        @Schema(description = "Dosagens e precauções", example = "1 comprimido a cada 8 horas")
         private String dosagesPrec;
 }
--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/entities/Appointment.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/entities/Appointment.java
@@ -1,5 +1,6 @@
 package com.labinc.Lab.Inc.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,32 +15,41 @@ import java.time.LocalTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Informações sobre a consulta")
 public class Appointment {
 
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "patient_id", referencedColumnName = "patient_id")
+    @Schema(description = "Paciente associado à consulta", requiredMode = Schema.RequiredMode.REQUIRED)
     private Patient patient;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID da consulta", example = "1")
     private Long appointmentId;
 
     @Column(nullable = false, length = 64)
+    @Schema(description = "Razão da consulta", requiredMode = Schema.RequiredMode.REQUIRED, example = "Rotina")
     private String reason;
 
     @Column(nullable = false)
+    @Schema(description = "Data da consulta", requiredMode = Schema.RequiredMode.REQUIRED, example = "15/10/2024")
     private LocalDate consultDate;
 
     @Column(nullable = false)
+    @Schema(description = "Hora da consulta", requiredMode = Schema.RequiredMode.REQUIRED, example = "14:30")
     private LocalTime consultTime;
 
     @Column(nullable = false, length = 1024)
+    @Schema(description = "Descrição do problema", requiredMode = Schema.RequiredMode.REQUIRED, example = "Dor de cabeça persistente")
     private String problemDescrip;
 
     @Column
+    @Schema(description = "Medicamentos prescritos", example = "Paracetamol")
     private String prescMed;
 
     @Column(length = 256)
+    @Schema(description = "Dosagens e precauções", example = "1 comprimido a cada 8 horas")
     private String dosagesPrec;
 
 }
