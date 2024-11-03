2f834be5bdc4bcb8ab52ef44dc2c05b274c0b2e1
Randy Gomes <raandy.gc@gmail.com>
2024-11-03 02:01:10 -0300

    docs(exceptions): adicionar documentação Swagger para Exceptions
    - Adicionar anotações @Operation e @ApiResponses para documentar os Exceptions da API.
    
    - Adicionar anotações @Schema nos Dtos referente à Exceptions.

--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/CustomError.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/CustomError.java
@@ -1,15 +1,22 @@
 package com.labinc.Lab.Inc.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 
+@Schema(description = "Informações sobre erros customizados")
 public class CustomError {
 
+    @Schema(description = "Timestamp do erro", example = "2024-01-01T12:00:00Z")
     private Instant timestamp;
 
+    @Schema(description = "Status HTTP do erro", example = "400")
     private Integer status;
 
+    @Schema(description = "Descrição do erro", example = "Bad Request")
     private String error;
 
+    @Schema(description = "Caminho da requisição que gerou o erro", example = "/api/resource")
     private String path;
 
     public CustomError(Instant timestamp, Integer status, String error, String path) {
