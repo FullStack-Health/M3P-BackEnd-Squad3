bd33b315118700ad92b474d082b51e80921b9ebe
Randy Gomes <raandy.gc@gmail.com>
2024-11-03 01:56:01 -0300

    docs(exam): adicionar documentação Swagger para Exam
    - Adicionar anotações @Operation e @ApiResponses para documentar endpoints da API.
    
    - Adicionar anotações @Schema na Entity e nos Dtos referentes à Exam.

--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/ExamResponseDTO.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/dtos/ExamResponseDTO.java
@@ -14,35 +14,36 @@ import java.time.LocalTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Dados de resposta para um exame")
 public class ExamResponseDTO {
 
-    @Schema(description = " Exam examName")
+    @Schema(description = "ID do exame", example = "1")
     private Long id;
 
-    @Schema(description = " Exam examName")
+    @Schema(description = "Nome do exame", example = "Exame de Sangue")
     private String examName;
 
-    @Schema(description = " Exam examDate")
+    @Schema(description = "Data do exame", example = "15/10/2024")
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate examDate;
 
-    @Schema(description = " Exam examTime")
+    @Schema(description = "Hora do exame", example = "14:30")
     @JsonFormat(pattern = "HH:mm")
     private LocalTime examTime;
 
-    @Schema(description = " Exam examType")
+    @Schema(description = "Tipo do exame", example = "Laboratorial")
     private String examType;
 
-    @Schema(description = " Exam lab")
+    @Schema(description = "Laboratório do exame", example = "Laboratório XYZ")
     private String lab;
 
-    @Schema(description = " Exam docUrl")
+    @Schema(description = "URL do documento do exame", example = "http://example.com/doc.pdf")
     private String docUrl;
 
-    @Schema(description = " Exam result")
+    @Schema(description = "Resultado do exame", example = "Negativo")
     private String result;
 
-    @Schema(description = " Exam patientId")
+    @Schema(description = "ID do paciente associado ao exame", example = "1")
     private Long patientId;
 
 
--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/entities/Exam.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/entities/Exam.java
@@ -1,5 +1,6 @@
 package com.labinc.Lab.Inc.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,32 +14,42 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "exams")
+@Schema(description = "Informações sobre o Exame")
 public class Exam {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID do exame", example = "1")
     private Long id;
 
     @Column(nullable = false, length = 64)
+    @Schema(description = "Nome do exame", requiredMode = Schema.RequiredMode.REQUIRED, example = "Exame de Sangue")
     private String examName;
 
     @Column(nullable = false)
+    @Schema(description = "Data do exame", requiredMode = Schema.RequiredMode.REQUIRED, example = "15/11/2024")
     private LocalDate examDate;
 
     @Column(nullable = false)
+    @Schema(description = "Hora do exame", requiredMode = Schema.RequiredMode.REQUIRED, example = "14:30")
     private LocalTime examTime;
 
     @Column(nullable = false, length = 32)
+    @Schema(description = "Tipo do exame", requiredMode = Schema.RequiredMode.REQUIRED, example = "Laboratorial")
     private String examType;
 
     @Column(nullable = false, length = 32)
+    @Schema(description = "Laboratório do exame", requiredMode = Schema.RequiredMode.REQUIRED, example = "Laboratório XYZ")
     private String lab;
 
+    @Schema(description = "URL do documento do exame", example = "http://example.com/doc.pdf")
     private String docUrl;
 
     @Column(length = 1024)
+    @Schema(description = "Resultado do exame", example = "Negativo")
     private String result;
 
+    @Schema(description = "Paciente associado ao exame", requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
