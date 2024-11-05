90a4916007c8bd5ebe4f97be6dfa12c79bb16869
GabrielFilomeno <biel_floripa@hotmail.com>
2024-11-05 16:46:47 -0300

    fix verificar dados ja existentes no edit paciente

--- Lab-Inc/src/main/java/com/labinc/Lab/Inc/services/PatientService.java
+++ Lab-Inc/src/main/java/com/labinc/Lab/Inc/services/PatientService.java
@@ -137,6 +137,24 @@ public class PatientService {
         User user = userRepository.findById(patient.getUser().getUserId())
                 .orElseThrow(() -> new ResourceNotFoundException("Usuário com ID: " + id + " não encontrado."));
 
+        if (!Objects.equals(patientRequestDTO.getCpf(), patient.getCpf())) {
+            if (patientRepository.existsByCpf(patientRequestDTO.getCpf()) || userRepository.existsByCpf(patientRequestDTO.getCpf())) {
+                throw new ResourceAlreadyExistsException("O CPF já está cadastrado: " + patientRequestDTO.getCpf());
+            }
+        }
+
+        if (!Objects.equals(patientRequestDTO.getRg(), patient.getRg())) {
+            if (patientRepository.existsByRg(patientRequestDTO.getRg())) {
+                throw new ResourceAlreadyExistsException("O RG já está cadastrado: " + patientRequestDTO.getRg());
+            }
+        }
+
+        if (!Objects.equals(patientRequestDTO.getEmail(), patient.getEmail())) {
+            if (patientRepository.existsByEmail(patientRequestDTO.getEmail()) || userRepository.existsByEmail(patientRequestDTO.getEmail())) {
+                throw new ResourceAlreadyExistsException("O Email já está cadastrado: " + patientRequestDTO.getEmail());
+            }
+        }
+
         PatientMapper.updatePatientFromDTO(patientRequestDTO, patient);
 
         userRepository.save(userMapper.updateUserFromPatientDto(user, patientRequestDTO));
